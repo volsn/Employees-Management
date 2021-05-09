057aa45b916f568e31d488eb00b0ad879c74dd27
Mykola Volosnikov <volosnikovkolia@gmail.com>
2021-05-09 20:29:49 +0300

    Added docstrings to model package

--- project/models/__init__.py
+++ project/models/__init__.py
@@ -7,23 +7,30 @@ from project import db
 
 class Department(db.Model):
     """
-    A class todo
+    A class for ORM to create 'Department' Table
 
     Attributes
     ----------
     name : str
         name of the department
+
+    Methods
+    -------
+        __init__(self, name: str) -> None:
+            Constructor with all the necessary attributes of the object.
+        __repr__(self) -> str:
+            Magic representation method of a class.
     """
 
     __tablename__ = 'department'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=True, nullable=False)
-    employees = db.relationship('Employee', backref='department')
+    employees = db.relationship('Employee', backref='department', passive_deletes=True)
 
     def __init__(self, name: str) -> None:
         """
-        Constructor with all the necessary attributes of the object
+        Constructor with all the necessary attributes of the object.
 
         Parameters
         ----------
@@ -34,7 +41,7 @@ class Department(db.Model):
 
     def __repr__(self) -> str:
         """
-        Magic representation method of a class
+        Magic representation method of a class.
 
         Returns
         -------
@@ -45,20 +52,57 @@ class Department(db.Model):
 
 
 class Employee(db.Model):
+    """
+    A class for ORM to create 'Department' Table
+
+    Attributes
+    ----------
+    name : str
+        name of the department
+
+    Methods
+    -------
+    __init__(self, name: str) -> None:
+        Constructor with all the necessary attributes of the object.
+    __repr__(self) -> str:
+        Magic representation method of a class.
+    """
 
     __tablename__ = 'employee'
 
     id = db.Column(db.Integer, primary_key=True)
-    department_id = db.Column(db.Integer, db.ForeignKey('department.id'), nullable=False)
+    department_id = db.Column(db.Integer, db.ForeignKey('department.id', ondelete='CASCADE'), nullable=False)
     name = db.Column(db.String(64), nullable=False)
     birthdate = db.Column(db.DateTime)
     salary = db.Column(db.Integer)
 
-    def __init__(self, department_id: int,  name: str, birthdate: datetime.date, salary: int) -> None:  # TODO
+    def __init__(self, department_id: int,  name: str, birthdate: datetime.date, salary: int) -> None:
+        """
+        Constructor with all the necessary attributes of the object.
+
+        Parameters
+        ----------
+            department_id : int
+                id of a department, where employee works. Used as a foreign key
+            name : str
+                name of the department. Has to be unique
+            birthdate : datetime.date
+                employee's date of birth
+            salary : int
+                employee's salary
+        """
         self.department_id = department_id
         self.name = name
         self.birthdate = birthdate
         self.salary = salary
 
     def __repr__(self):
+        """
+        Magic representation method of a class.
+
+        Returns
+        -------
+            name : str
+                name of the employee
+        """
         return self.name
--- project/models/populate.py
+++ project/models/populate.py
@@ -1,25 +1,54 @@
-import os
+"""
+Populate App Database
+
+Functions:
+
+    drop_data() -> None
+    populate_departments(limit: int = 14) -> None
+    populate_departments(limit: int = 14) -> None
+    main() -> None
+"""
 import random
 import logging
 import argparse
+
+logging.basicConfig(level=logging.DEBUG, filename='app.log')
+
 from faker import Faker
 
-from project import db, basedir
+from project import db
 from project.models import Department, Employee
 
 
-def drop_data():
+def drop_data() -> None:
+    """
+    Drop Data from the Tables.
+
+    Raises:
+        SQLAlchemy Exception : May raise when error occurs while working with the DB.
+    """
     try:
-        db.session.query(Department).delete()
-        db.session.query(Employee).delete()
+        Department.query.delete()
+        Employee.query.delete()
         db.session.commit()
+
     except:
         logging.error('Couldn\'t delete Data from the Database.')
         db.session.rollback()
 
 
-def populate_departments(limit=14):
+def populate_departments(limit: int = 14) -> None:
+    """
+    Populates 'Department' Table
+
+    Parameters:
+        limit (int): Max number of departments
+
+    Returns:
+        None
+    """
     logging.info('Populating Departments Database')
+
     department_names = ['General Management', 'Marketing', 'Operations',
                         'Finance', 'Sales', 'Human Resource', 'Purchase',
                         'Training', 'Development', 'Test Team', 'Architecture',
@@ -29,15 +58,28 @@ def populate_departments(limit=14):
         department = Department(name=name)
         db.session.add(department)
     db.session.commit()
+
     logging.info('Successfully populated Departments Database')
 
 
-def populate_employees(max_limit_per_department=10):
+def populate_employees(limit: int = 10) -> None:
+    """
+    Populates 'Employees' Table
+
+    Parameters:
+        limit (int): Max number of employees per department
+
+    Returns:
+        None
+    """
     logging.info('Populating Employees Database')
+
+    Faker.seed(42)
     faker = Faker()
+
     departments = Department.query.all()
     for department in departments:
-        for _ in range(random.randint(1, max_limit_per_department)):
+        for _ in range(random.randint(1, limit)):
             employee = Employee(
                 department_id=department.id,
                 name=faker.name(),
@@ -46,9 +88,28 @@ def populate_employees(max_limit_per_department=10):
             )
             db.session.add(employee)
     db.session.commit()
+
     logging.info('Successfully populated Employees Database')
 
 
+def main() -> None:
+    """ Main function that runs when the Script is called from the Console """
+    parser = argparse.ArgumentParser(description='A Script to populate App '
+                                                 'Database with the test Data.')
+    parser.add_argument('-d', '--drop', help='Drop Data from the Tables.',
+                        action='store_true')
+    parser.add_argument('-ld', '--limit-departments', default=14,
+                        help='Limit Number of Departments', type=int)
+    parser.add_argument('-le', '--limit-employees', default=10,
+                        help='Limit Number of Employees per Department', type=int)
+    args = parser.parse_args()
+
+    if args.drop:
+        drop_data()
+
+    populate_departments(args.limit_departments)
+    populate_employees(args.limit_employees)
+
+
 if __name__ == '__main__':
-    populate_departments()
-    populate_employees()
+    main()
